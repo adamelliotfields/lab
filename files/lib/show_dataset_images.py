96700c75dd577ad2ea762d289ec193cf25e6666e
Adam Fields <me@aef.me>
2024-06-03 17:30:23 -0400

    Add CIFAR notebook

--- files/lib/show_dataset_images.py
+++ files/lib/show_dataset_images.py
@@ -1,19 +1,19 @@
 import io
 
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 from IPython.display import display_jpeg
-from keras import ops
 
 
 # displays images from a TensorFlow Dataset
 def show_dataset_images(
     ds,
+    *,
     rows=1,
     size=2,
     columns=5,
     figsize=None,
-    unbatch=False,
     label_decoder=None,
 ):
     if figsize is None:
@@ -21,18 +21,31 @@ def show_dataset_images(
 
     fig, axes = plt.subplots(rows, columns, figsize=figsize)
 
-    if unbatch:
-        ds = ds.unbatch()
-
     for i, (image, label) in enumerate(ds.take(rows * columns)):
-        cmap = "gray" if image.shape[-1] == 1 else None
+        image = image.numpy().squeeze()
+
+        # rescale from -1, 1 to 0, 1
+        if image.min() < 0:
+            image = (image + 1) / 2
+
+        # rescale to 0, 255 integers
+        if not image.max() > 1:
+            image = image * 255
+
+        image = image.astype("uint8")
         ax = axes[i // columns, i % columns] if rows > 1 else axes[i]
-        ax.imshow(ops.squeeze(image), cmap=cmap)
-        if label_decoder is not None:
-            ax.set_title(label_decoder(ops.squeeze(label)))
-        else:
-            ax.set_title(ops.argmax(label).numpy() if label.shape.ndims > 0 else label.numpy())
+        ax.imshow(image, cmap="gray" if image.shape[-1] == 1 else None)
         ax.axis("off")
+
+        # if one-hot encoded
+        label = tf.squeeze(label)
+        if tf.rank(label) > 0:
+            label = tf.argmax(label)
+
+        if label_decoder is not None:
+            ax.set_title(label_decoder(label))
+        else:
+            ax.set_title(f"{label}")
 
     # jpeg compress the figure to significantly reduce notebook size
     fig_buf = io.BytesIO()
